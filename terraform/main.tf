2027194571cb0fe9608665311106ddc4c2e1bc18
boughamoura mohamed alaa <boughamouramohamedalaa2@gmail.com>
2026-04-23 20:06:17 +0100

    feat: full AWS deployment working - WebSocket confirmed, S3 media pending fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -301,6 +301,7 @@ resource "aws_instance" "main" {
   subnet_id              = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
   key_name               = aws_key_pair.main.key_name
+  iam_instance_profile   = "LabInstanceProfile"
 
   root_block_device {
     volume_size = 20
@@ -331,6 +332,11 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
+  stickiness {
+    type    = "lb_cookie"
+    enabled = true
+  }
+
   health_check {
     path                = "/"
     healthy_threshold   = 2
@@ -351,9 +357,28 @@ resource "aws_lb_listener" "main" {
   }
 }
 
+# WebSocket listener rule
+resource "aws_lb_listener_rule" "websocket" {
+  listener_arn = aws_lb_listener.main.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/ws/*"]
+    }
+  }
+}
+
 # Attach EC2 to Target Group
 resource "aws_lb_target_group_attachment" "main" {
   target_group_arn = aws_lb_target_group.main.arn
   target_id        = aws_instance.main.id
   port             = 80
 }
+
+ 
